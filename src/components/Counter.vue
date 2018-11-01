0ebb09ff7db53f3d2bd8c35dfa9f2ddf64c6c6f5
unknown <judit.pacsai@gmail.com>
2018-11-01 22:53:24 +0100

    feat: first click event

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="doStuff">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    msg: String
+  },
+  methods: {
+    doStuff () {
+      alert('Did something!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
