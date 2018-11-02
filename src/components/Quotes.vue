cd522e8eefb032344057d03d959441f495dc19da
unknown <judit.pacsai@gmail.com>
2018-11-02 22:42:39 +0100

    style: remove unnecessary code

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,65 +1,60 @@
 <template>
-  <div class="block">
-    <h1>{{ msg }}</h1>
-    <h3>Chuck Norris jokes</h3>
-    <p id="quotes">{{ this.quote }}</p>
-    <button @click='getQuote'>Get a new joke</button>
-  </div>
+	<div class="block">
+		<h1>{{ msg }}</h1>
+		<h3>Chuck Norris jokes</h3>
+		<p id="quotes">{{ this.quote }}</p>
+		<button @click='getQuote'>Get a new joke</button>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'Quotes',
-  props: {
-    msg: String
-  },
-  data () {
-    return {
-      quote: ''
-    }
-  },
-  methods: {
-    getQuote() {
-        fetch('https://api.chucknorris.io/jokes/random').then(data => {
-            return data.json();
-            //this.quote = data
-        }).then(data => {
-            console.log(data.value);
-            this.quote = data.value;
-        });
-    }
-  },
-  created: function() {
-        this.getQuote();
-    }
-}
+	export default {
+	name: "Quotes",
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+		quote: ""
+		};
+	},
+	methods: {
+		getQuote() {
+		fetch("https://api.chucknorris.io/jokes/random")
+			.then(data => data.json())
+			.then(data => (this.quote = data.value));
+		}
+	},
+	created: function() {
+		this.getQuote();
+	}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .block {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+	.block {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-    h3 {
-        margin: 0;
-    }
+	h3 {
+		margin: 0;
+	}
 
-    p {
-        width: 200px;
-        background-color: white;
-        padding: 15px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+	p {
+		width: 200px;
+		background-color: white;
+		padding: 15px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-    button {
-        width: 80px;
-    }
-
+	button {
+		width: 80px;
+	}
 </style>
