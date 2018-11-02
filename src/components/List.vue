e1b37f6d65ca261b1a35313eff005390d1f3b267
unknown <judit.pacsai@gmail.com>
2018-11-02 15:45:27 +0100

    style: li grid layout

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,7 +7,8 @@
 	</form>
 	<ul id='list'>
 		<li v-for="l in list" :key="l.id">
-			{{ l.text }}
+			<span>{{ l.text }}</span>
+			<button>Delete</button>
 		</li>
 	</ul>
 </div>
@@ -15,63 +16,76 @@
 
 <script>
 export default {
-name: "List",
-props: {
-	msg: String
-},
-data() {
-	return {
-		listItem: "",
-		list: [],
-		id: 0
-	};
-},
-methods: {
-	add() {
-		const listObj = {
-			text: this.listItem,
-			id: this.id
-		}
-		this.list.push(listObj);
-		this.listItem = "";
-		this.id++;
-	}
-}
+  name: "List",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      listItem: "",
+      list: [],
+      id: 0
+    };
+  },
+  methods: {
+    add() {
+      const listObj = {
+        text: this.listItem,
+        id: this.id
+      };
+      this.list.push(listObj);
+      this.listItem = "";
+      this.id++;
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list-container {
-display: flex;
-justify-content: center;
-padding-bottom: 15px;
+form {
+  align-items: center;
+  display: grid;
+  grid-template: 100% / 200px 80px;
+  padding-bottom: 15px;
+  justify-content: center;
 }
 
 input {
-padding: 15px;
-box-sizing: border-box;
-height: 40px;
+  padding: 15px;
+  box-sizing: border-box;
+  height: 40px;
 }
 
 button {
-height: 40px;
-width: 60px;
-margin-left: 10px;
-display: flex;
-align-items: center;
-justify-content: center;
-background-color: darkorchid;
-color: white;
+  height: 40px;
+  width: 60px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: darkorchid;
+  color: white;
 }
 
 ul {
-width: 50%;
-margin-left: 40px;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 li {
-text-align: left;
-margin: 5px auto;
+  text-align: left;
+  display: grid;
+  grid-template: 100% / 200px 80px;
+  margin: 5px auto;
+  justify-content: center;
+}
+
+span {
+	display: flex;
+    align-items: center;
 }
 </style>
