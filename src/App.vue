f993d47ed34d77bca2abdc672099fdffe6ed5c5e
unknown <judit.pacsai@gmail.com>
2018-11-02 15:54:55 +0100

    style: slim scroll bar

--- src/App.vue
+++ src/App.vue
@@ -6,51 +6,71 @@
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
-import List from './components/List';
+import Counter from "./components/Counter.vue";
+import List from "./components/List";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Counter,
     List
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-.block {
-  width: 400px;
-  padding: 15px;
-  background-color: lavender;
-  margin: 15px;
-}
+	.block {
+		width: 400px;
+		padding: 15px;
+		background-color: lavender;
+		margin: 15px;
+	}
 
-h3 {
-  margin: 40px 0 0;
-}
+	h3 {
+		margin: 40px 0 0;
+	}
 
-input, button, submit {
-  border: none;
-} 
+	input, button, submit {
+		border: none;
+	}
 
-button {
-  background-color: darkorchid;
-  color: white;
-}
+	button {
+		background-color: darkorchid;
+		color: white;
+	}
 
-button:active {
-  background-color: rgb(72, 15, 100);
+	button:active {
+		background-color: rgb(72, 15, 100);
+	}
+
+	::-webkit-scrollbar {
+    	height: 8px;
+		width: 8px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #f1f1f1; 
+	}
+	
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888; 
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555; 
 }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -16,76 +16,77 @@
 
 <script>
 export default {
-  name: "List",
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      listItem: "",
-      list: [],
-      id: 0
-    };
-  },
-  methods: {
-    add() {
-      const listObj = {
-        text: this.listItem,
-        id: this.id
-      };
-      this.list.push(listObj);
-      this.listItem = "";
-      this.id++;
-    }
-  }
+	name: "List",
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+		listItem: "",
+		list: [],
+		id: 0
+		};
+	},
+	methods: {
+		add() {
+		const listObj = {
+			text: this.listItem,
+			id: this.id
+		};
+		this.list.push(listObj);
+		this.listItem = "";
+		this.id++;
+		}
+	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-form {
-  align-items: center;
-  display: grid;
-  grid-template: 100% / 200px 80px;
-  padding-bottom: 15px;
-  justify-content: center;
-}
+	form {
+		align-items: center;
+		display: grid;
+		grid-template: 100% / 200px 80px;
+		padding-bottom: 15px;
+		justify-content: center;
+	}
 
-input {
-  padding: 15px;
-  box-sizing: border-box;
-  height: 40px;
-}
+	input {
+		padding: 15px;
+		box-sizing: border-box;
+		height: 40px;
+	}
 
-button {
-  height: 40px;
-  width: 60px;
-  margin-left: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: darkorchid;
-  color: white;
-}
+	button {
+		height: 40px;
+		width: 60px;
+		margin-left: 10px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: darkorchid;
+		color: white;
+	}
 
-ul {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+	ul {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-li {
-  text-align: left;
-  display: grid;
-  grid-template: 100% / 200px 80px;
-  margin: 5px auto;
-  justify-content: center;
-}
+	li {
+		text-align: left;
+		display: grid;
+		grid-template: 100% / 200px 80px;
+		margin: 5px auto;
+		justify-content: center;
+	}
 
-span {
-	display: flex;
-    align-items: center;
-}
+	span {
+		display: flex;
+		align-items: center;
+		overflow: auto;
+	}
 </style>
